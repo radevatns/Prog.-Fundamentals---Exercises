cf7eab697588eb3b8012956f4750bd607eff7c2a
Nasko <radevatns@gmail.com>
2016-09-22 12:24:27 +0300

    add a 1st exercise

---
+++ 01GitHub/ConsoleApplication1/HolidaysBetweenTwoDates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class HolidaysBetweenTwoDates
+    {
+        static void Main()
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(startDate);
+            Console.WriteLine(endDate);
+            var holidaysCount = 0;
+            var curentTime = DateTime.Now;//del if not used
+            Console.WriteLine("Current Time is {0}", curentTime);
+
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                Console.WriteLine("the date is: {0}", date);
+                               
+                if (date.DayOfWeek == DayOfWeek.Saturday ||
+                        date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount++;
+                }
+            }
+            Console.WriteLine(holidaysCount);
+        }
+     
+    }
+}
+
+/*var startDate = DateTime.ParseExact(Console.ReadLine(),
+                        "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var holidaysCount = 0;
+            for (var date = startDate; date <= endDate; date.AddDays(1))
+                if (date.DayOfWeek == DayOfWeek.Saturday &&
+                    date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
+            Console.WriteLine(holidaysCount);
+ */
