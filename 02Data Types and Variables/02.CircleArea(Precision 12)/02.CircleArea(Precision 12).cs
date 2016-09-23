888c9088d2a91223d0f3c36a26895a2ddcbea8ee
Nasko <radevatns@gmail.com>
2016-09-23 14:08:38 +0300

    02 was OK

---
+++ 02Data Types and Variables/02.CircleArea(Precision 12)/02.CircleArea(Precision 12).cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        var radius = double.Parse(Console.ReadLine());
+        var area = Math.PI * (radius * radius);
+        Console.WriteLine("{0:f12}", area);
+        }
+    }
+
