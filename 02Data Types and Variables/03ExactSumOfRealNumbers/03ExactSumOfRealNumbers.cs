498e595411b01a7aa98852c7333482ff3a6242b0
Nasko <radevatns@gmail.com>
2016-09-23 14:47:03 +0300

    ready with 04

---
+++ 02Data Types and Variables/03ExactSumOfRealNumbers/03ExactSumOfRealNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        var numbers = int.Parse(Console.ReadLine());
+        decimal sum = 0;
+        for (int i = 0; i < numbers; i++)
+            {
+                decimal customNum  = decimal.Parse(Console.ReadLine());
+                sum += customNum;
+            }  
+        Console.WriteLine(sum);
+        }
+    }
---
+++ 02Data Types and Variables/04Elevator/04Elevator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        var n = decimal.Parse(Console.ReadLine());//count of people 
+        var p = decimal.Parse(Console.ReadLine());// capacity of elevator
+        var cources = Math.Ceiling(n / p);
+        Console.WriteLine(cources);
+        }
+    }
