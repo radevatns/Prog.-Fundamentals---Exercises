1557df14348336e2b36d7304fe665a5838c5c5b8
Nasko <radevatns@gmail.com>
2016-09-25 22:34:51 +0300

    add solution for 08

--- 02Data Types and Variables/07Greeting/07Greeting.cs
+++ 02Data Types and Variables/07Greeting/07Greeting.cs
@@ -3,6 +3,10 @@
     class Program
     {
         static void Main(string[] args)
-        {
-        }
+    {
+        string firstName = Console.ReadLine();
+        string secondName = Console.ReadLine();
+        var years = int.Parse(Console.ReadLine());
+        Console.WriteLine("Hello,{0} {1}. You are {2} years old",firstName,secondName,years);
     }
+}
---
+++ 02Data Types and Variables/08Refactor Volume of Pyramid/08Refactor Volume of Pyramid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        double lenght, width, height;
+        Console.Write("Length: ");
+        lenght = double.Parse(Console.ReadLine());
+        Console.Write("Width: ");
+        width = double.Parse(Console.ReadLine());
+        Console.Write("Height: ");
+        height = double.Parse(Console.ReadLine());
+        double volume = ((lenght * width) * height) / 3;
+        Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+
+    }
+}
+
