666b201eb7cc087fe83d3fd3ea3283db5eaec80a
Nasko <radevatns@gmail.com>
2016-09-25 23:04:43 +0300

    solution for 09 add folder for 02 for exercises

--- 02Data Types and Variables/09Refactor Special Numbers/09Refactor Special Numbers.cs
+++ 02Data Types and Variables/09Refactor Special Numbers/09Refactor Special Numbers.cs
@@ -2,8 +2,24 @@
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+        int n = int.Parse(Console.ReadLine());
+
+        bool result = false;
+        for (int i = 1; i <= n; i++)
+        {
+            int sum = 0;
+            int tempNum = i;
+            while (tempNum > 0)
+            {
+                sum += tempNum % 10;
+                tempNum = tempNum / 10;
+            }
+            result = (sum == 5) || (sum == 7) || (sum == 11);
+            Console.WriteLine($"{i} -> {result}");
+            sum = 0;
         }
     }
 }
+
