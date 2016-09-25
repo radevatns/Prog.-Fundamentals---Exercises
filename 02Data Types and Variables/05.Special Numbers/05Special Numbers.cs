35836044eb8c668f08b0959563eaf8401e21d891
Nasko <radevatns@gmail.com>
2016-09-25 20:27:20 +0300

    add solution for 05,06

---
+++ 02Data Types and Variables/05.Special Numbers/05Special Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {
+        var n = int.Parse(Console.ReadLine());
+        //Console.WriteLine(n%10);
+        for (int i = 1; i <= n; i++)
+        {
+            int num = i;
+            int sum = 0;
+            int lastDigit = 0;
+            while (num !=0)
+            {
+                lastDigit = num % 10;// return last digit
+                sum = sum + lastDigit; // add to result
+                num = num / 10;
+            }
+            if (sum == 5 ||sum == 7 || sum == 11 )
+            {
+                Console.WriteLine("{0} -> True", i);
+            }
+            else
+            {
+                Console.WriteLine("{0} -> False", i);
+            }            
+        }
+    }
+    }
---
+++ 02Data Types and Variables/06Triples of Letters/06Triples of Letters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        for (int outSide = 0; outSide < n; outSide++)
+        {
+            char letterOut = (char)('a' + outSide);
+
+            for (int mid = 0; mid < n; mid++)
+            {
+                char letterMid = (char)('a' + mid);
+
+                for (int inSide = 0; inSide < n; inSide++)
+                {
+                    char letterIn = (char)('a' + inSide);
+
+                    Console.WriteLine("{0}{1}{2}", letterOut, letterMid, letterIn);
+                }
+            }
+        }
+    }
+}
