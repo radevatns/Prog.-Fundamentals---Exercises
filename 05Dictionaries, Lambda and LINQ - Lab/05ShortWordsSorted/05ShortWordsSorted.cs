2e6e6884e0b546152bda1581d3503a0ef8acef89
Nasko <radevatns@gmail.com>
2016-10-06 17:06:11 +0300

    add solution for 05 and 06

--- 05Dictionaries, Lambda and LINQ - Lab/05ShortWordsSorted/05ShortWordsSorted.cs
+++ 05Dictionaries, Lambda and LINQ - Lab/05ShortWordsSorted/05ShortWordsSorted.cs
@@ -10,6 +10,25 @@ namespace _05ShortWordsSorted
     {
         static void Main(string[] args)
         {
+            /*var separators = ". , : ; ( ) [ ] \" ' \\ / ! ?  .".ToArray();
+            var inputList = Console.ReadLine().ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var distinctList = inputList.Distinct();
+            var lengthList = distinctList.Where(s => s.Length < 5);
+            var sortedList = lengthList.OrderBy(x => x);
+            Console.WriteLine(string.Join(", ", sortedList));*/
+            // option 2
+            char[] separators = new char[]{'.',',',':',';','(',')','[',']','\\','\"','\'','/','!','?',' '};
+            // or char[] separators = ".,:;()[]\"'!? ".ToCharArray();
+            string sentence = Console.ReadLine().ToLower();
+            string[] words = sentence.Split(separators);
+            var result = words
+                .Where(w => w != "")
+                .Where(w => w.Length < 5)
+                .OrderBy(w => w)
+                .Distinct(); // remove repetition (povtorenie)
+            Console.WriteLine(string.Join(", ", result));
+
+
         }
     }
 }
--- 05Dictionaries, Lambda and LINQ - Lab/06FoldandSum/06FoldandSum.cs
+++ 05Dictionaries, Lambda and LINQ - Lab/06FoldandSum/06FoldandSum.cs
@@ -10,6 +10,19 @@ namespace _06FoldandSum
     {
         static void Main(string[] args)
         {
-        }
+                int[] arr = Console.ReadLine()
+                .Split(' ').Select(int.Parse).ToArray();
+                int k = arr.Length / 4;
+                var row1left = arr.Take(k).Reverse();
+                var row1right = arr.Reverse().Take(k);
+                int[] row1 = row1left.Concat(row1right).ToArray();//concat saedinqva 2 spisaka
+                int[] row2 = arr.Skip(k).Take(2 * k).ToArray();
+                var sumArr =
+                  row1.Select((x, index) => x + row2[index]);
+                Console.WriteLine(string.Join(" ", sumArr));
+
+         }
     }
+
+
 }
